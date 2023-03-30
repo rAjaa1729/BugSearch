4f7e8509d89403c4ddd749833fb92a2ef6dbd00f
Raja Prism <rajaprismsagar001@gmail.com>
2023-03-31 01:21:32 +0530

    updated

---
+++ database.sql
@@ -0,0 +1,105 @@
+CREATE DATABASE BugSearch;
+USE BugSearch;
+
+-- TABLE FOR USER
+CREATE TABLE Users
+(
+    user_id INT NOT NULL AUTO_INCREMENT,
+    passcode INT NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    email_id VARCHAR(30) NOT NULL,
+    creation_date DATE NOT NULL,
+    profile_image_url VARCHAR(30),
+    reputation INT NOT NULL DEFAULT 0,
+    about TEXT,
+    badge VARCHAR(30) NOT NULL DEFAULT "newbie",
+    nfollowers INT NOT NULL DEFAULT 0,
+    nfollowing INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (user_id)
+);
+
+-- TABLE FOR Answers
+CREATE TABLE Answers
+(
+    answer_id INT NOT NULL AUTO_INCREMENT,
+    body TEXT NOT NULL,
+    question_id INT NOT NULL,
+    user_id INT NOT NULL,
+    score INT DEFAULT 0,
+    creation_date DATE NOT NULL,
+    comment_count INT NOT NULL DEFAULT 0,
+    upvotes INT NOT NULL DEFAULT 0,
+    downvotes INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (answer_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (question_id) REFERENCES Questions(question_id)
+);
+
+-- table for questions
+CREATE TABLE Questions
+(
+    question_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100),
+    body TEXT NOT NULL,
+    answer_accepted_id INT ,
+    user_id INT NOT NULL,
+    score INT NOT NULL DEFAULT 0,
+    creation_date DATE NOT NULL,
+    comment_count INT NOT NULL DEFAULT 0,
+    answer_count INT NOT NULL DEFAULT 0,
+    upvotes INT NOT NULL DEFAULT 0,
+    downvotes INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (question_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (answer_id) REFERENCES Answers(answer_id)
+);
+
+CREATE TABLE Comments
+(
+    comment_id INT NOT NULL AUTO_INCREMENT,
+    body TEXT NOT NULL,
+    creation_date DATE NOT NULL,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    post_type INT NOT NULL,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (post_id)
+);
+
+CREATE TABLE Votes
+(
+    vote_id INT NOT NULL AUTO_INCREMENT,
+    vote_type INT NOT NULL,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    post_type INT NOT NULL,
+    PRIMARY KEY (vote_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (post_id)
+);
+
+CREATE TABLE Tags
+(
+    tag_id INT NOT NULL AUTO_INCREMENT,
+    tag_name VARCHAR(20) NOT NULL,
+    about TEXT NOT NULL
+);
+
+CREATE TABLE Follwertags
+(
+    follower_id INT NOT NULL,
+    following_id INT NOT NULL
+);
+
+CREATE TABLE Usertags
+(
+    tag_id INT NOT NULL,
+    user_id INT NOT NULL
+);
+
+CREATE TABLE Questiontags
+(
+    tag_id INT NOT NULL,
+    question_id INT NOT NULL
+);
