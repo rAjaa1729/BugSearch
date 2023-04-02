80b92f3612e74855cb6c8616129b326a15c6a3ca
Raja Prism <rajaprismsagar001@gmail.com>
2023-04-02 22:31:19 +0530

    new

--- database.sql
+++ database.sql
@@ -31,8 +31,7 @@ CREATE TABLE Answers
     upvotes INT NOT NULL DEFAULT 0,
     downvotes INT NOT NULL DEFAULT 0,
     PRIMARY KEY (answer_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (question_id) REFERENCES Questions(question_id)
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) 
 );
 
 -- table for questions
@@ -41,7 +40,7 @@ CREATE TABLE Questions
     question_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(100),
     body TEXT NOT NULL,
-    answer_accepted_id INT ,
+    answer_id INT ,
     user_id INT NOT NULL,
     score INT NOT NULL DEFAULT 0,
     creation_date DATE NOT NULL,
@@ -53,7 +52,10 @@ CREATE TABLE Questions
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (answer_id) REFERENCES Answers(answer_id)
 );
+ALTER TABLE Answers ADD FOREIGN KEY (question_id) REFERENCES Questions(question_id) ON DELETE CASCADE;
 
+
+--
 CREATE TABLE Comments
 (
     comment_id INT NOT NULL AUTO_INCREMENT,
@@ -61,22 +63,24 @@ CREATE TABLE Comments
     creation_date DATE NOT NULL,
     user_id INT NOT NULL,
     post_id INT NOT NULL,
-    post_type INT NOT NULL,
+    post_type ENUM('question', 'answer') NOT NULL,
     PRIMARY KEY (comment_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (post_id)
+    FOREIGN KEY (post_id) REFERENCES Questions(question_id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES Answers(answer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Votes
 (
     vote_id INT NOT NULL AUTO_INCREMENT,
-    vote_type INT NOT NULL,
+    vote_type ENUM('upvote', 'downvote') NOT NULL,
     post_id INT NOT NULL,
     user_id INT NOT NULL,
-    post_type INT NOT NULL,
+    post_type ENUM('question', 'answer') NOT NULL,
     PRIMARY KEY (vote_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (post_id)
+    FOREIGN KEY (post_id) REFERENCES Questions(question_id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES Answers(answer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Tags
@@ -103,3 +107,31 @@ CREATE TABLE Questiontags
     tag_id INT NOT NULL,
     question_id INT NOT NULL
 );
+
+
+----
+CREATE TRIGGER update_badge
+BEFORE UPDATE ON Users 
+FOR EACH ROW
+BEGIN
+        IF NEW.reputation >= 20 AND NEW.reputation < 40 THEN
+            SET NEW.badge = 'Pupil';
+        ELSEIF NEW.reputation >= 40 AND NEW.reputation < 70 THEN
+            SET NEW.badge = 'Specialist';
+        ELSEIF NEW.reputation >= 70 AND NEW.reputation < 100 THEN
+            SET NEW.badge = 'Expert';
+        ELSEIF NEW.reputation >= 100 AND NEW.reputation < 120 THEN
+            SET NEW.badge = 'Candidate Master';
+        ELSEIF NEW.reputation >= 120 AND NEW.reputation < 130 THEN
+            SET NEW.badge = 'Master';
+        ELSEIF NEW.reputation >= 130 AND NEW.reputation < 140 THEN
+            SET NEW.badge = 'International Master';
+        ELSEIF NEW.reputation >= 140 AND NEW.reputation < 150 THEN
+            SET NEW.badge = 'Grandmaster';
+        ELSEIF NEW.reputation >= 150 AND NEW.reputation < 200 THEN
+            SET NEW.badge = 'International Grandmaster';
+        ELSEIF NEW.reputation >= 200 THEN
+            SET NEW.badge = 'Legendary Grandmaster';
+        END IF;
+    END IF;
+END;
