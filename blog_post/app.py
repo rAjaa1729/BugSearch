675e5a692bffae344c9f74fb7c56a55fb8d8245a
Raja Prism <rajaprismsagar001@gmail.com>
2023-04-04 09:30:20 +0530

    making blog post

--- blog_post/app.py
+++ blog_post/app.py
@@ -1,9 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 import mysql.connector
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'your secret key'
 def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
@@ -39,4 +39,23 @@ def index():
     cursor.close()
     connection.close()
     return render_template('index.html', posts=posts)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            connection = get_db_connection()
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute('INSERT INTO post (title, content) VALUES (%s,%s )',(title, content))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
 
